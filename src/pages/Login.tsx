b80cd2349253f44a1e2f6a132c2af555df80f1f3
lisar <lisar.3467@gmail.com>
2022-04-25 17:16:45 -0300

    new login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,46 @@
+import {useState} from "react";
+import {Box, Button, Form, FormField} from "grommet";
 
-const Login = () =>
-    <h1>Inicio de sesión</h1>;
+interface FormState {
+    username?: string,
+    password?: string
+}
+
+const Login = () => {
+    const [form, setForm] = useState<FormState>({
+        username: "",
+        password: ""
+    });
+
+    return <Box fill align="center" justify="center">
+        <Box width="medium">
+            <Form<FormState>
+                value={form}
+                messages={{required: "Requerido*"}}
+                onReset={() => setForm({})}
+                onChange={(nextForm, _) => {
+                    setForm(nextForm)
+                }}
+                onSubmit={({value}) => console.log("onSubmit", value)}
+            >
+                <FormField
+                    label="Nombre de usuario"
+                    name="username"
+                    required
+                />
+                <FormField
+                    label="Contraseña"
+                    name="password"
+                    type="password"
+                    required
+                />
+                <Box direction="row" justify="between" margin={{top: "medium"}}>
+                    <Button type="submit" label="Ingresar" primary/>
+                </Box>
+            </Form>
+        </Box>
+    </Box>
+}
 
 export default Login;
+
