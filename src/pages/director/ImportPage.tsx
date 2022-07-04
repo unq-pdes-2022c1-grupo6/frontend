2adfca0de64facdc7e7e99993a8f343030b651a3
lisar <lisar.3467@gmail.com>
2022-07-03 23:51:07 -0300

    installed papaparse, starting import page

--- src/pages/director/ImportPage.tsx
+++ src/pages/director/ImportPage.tsx
@@ -1,9 +1,37 @@
+import {Box, Button, FileInput, Page, PageContent, Tab, Tabs} from "grommet";
+import {useState} from "react";
+import Papa from 'papaparse';
 
 
 const ImportPage = () => {
-    return <h1>
-        Pagina de importación
-    </h1>
+    const [activeTab, setActiveTab] = useState(0);
+    const [file, setFile] = useState<File | undefined>();
+
+    return <Page kind="narrow" pad="large" align="center">
+        <PageContent>
+            <Tabs alignControls="start" justify="start" activeIndex={activeTab} onActive={setActiveTab}>
+                <Tab title="Alumnos">
+                    <Box pad="small">
+                        <FileInput
+                            onChange={(event, value) => {
+                                if (value?.files) {
+                                    const newFile = Object.values(value.files)[0];
+                                    setFile(newFile);
+                                    console.log(newFile);
+                                }
+                            }}/>
+                        <Button onClick={() => {
+                            if (file) {
+                                Papa.parse(file, {
+                                    complete: (data) => console.log(data)
+                                })
+                            }
+                        }}/>
+                    </Box>
+                </Tab>
+            </Tabs>
+        </PageContent>
+    </Page>
 
 };
 
