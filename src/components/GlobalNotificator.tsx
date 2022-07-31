dac1d02ce54e674937821135e21a4f26cbfae14f
lisar01 <lisar.3467@gmail.com>
2022-07-31 17:56:34 -0300

    notificator is a toast now, missing file

--- src/components/GlobalNotificator.tsx
+++ src/components/GlobalNotificator.tsx
@@ -1,34 +1,13 @@
-import {Notification, NotificationProps} from 'grommet';
-import {useEffect} from "react";
+import {Notification, NotificationProps} from "grommet";
 import isEmpty from "lodash/isEmpty";
 
-type GlobalNotificatorProps = {
-    notification?: NotificationProps,
-    onCloseNotification?: () => void
-}
 
-// los parametros nunca van a estar vacios, es un problema de usar context
-const GlobalNotificator = ({notification = {}, onCloseNotification}: GlobalNotificatorProps) => {
+const GlobalNotificator = ({onClose = () => {}, ...props}: NotificationProps) => {
 
-        useEffect(() => {
-                const timer = setTimeout(() => {
-                    onCloseNotification && onCloseNotification();
-                }, 10000);
-                return () => {
-                    clearTimeout(timer);
-                };
-            }, [notification, onCloseNotification]
-        )
+    return !isEmpty(props) ?
+        <Notification toast {...props} onClose={onClose}/> :
+        <></>
 
-return !isEmpty(notification) ?
-    <Notification
-        global
-        {...notification}
-        onClose={() => onCloseNotification && onCloseNotification()}
-    /> :
-    <></>
-
-}
-;
+};
 
 export default GlobalNotificator;
